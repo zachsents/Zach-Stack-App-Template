8cb86c1a681b8a36a57bbc81bde3dfd20e722cfc
Zach Sents <zachsents@gmail.com>
2023-11-27 18:25:46 -0700

    Fixed up _document

--- web/pages/_document.js
+++ web/pages/_document.js
@@ -1,6 +1,8 @@
 import { additionalCSSVariables } from "@web/theme"
 import { Head, Html, Main, NextScript } from "next/document"
 
+const headline = "xxx"
+const description = "xxx"
 
 export default function Document() {
 
@@ -11,33 +13,39 @@ export default function Document() {
                 <meta charSet="UTF-8" />
                 <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
 
-                {/* Title & Description */}
-                <title key="title">xxx</title>
+                {/* Meta Description */}
                 <meta
                     name="description"
-                    content="xxx"
+                    content={description}
                     key="description"
                 />
 
                 {/* Favicon & other icons */}
-                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-                <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-                <link rel="manifest" href="/site.webmanifest" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/assets/apple-touch-icon.png" />
+                <link rel="icon" href="/assets/favicon.ico" type="image/x-icon" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon-16x16.png" />
+                <link rel="manifest" href="/assets/site.webmanifest" />
 
                 {/* OpenGraph */}
                 <meta
                     property="og:title"
-                    content="xxx"
+                    content={headline}
                     key="ogtitle"
                 />
                 <meta
                     property="og:description"
-                    content="xxx"
+                    content={description}
                     key="ogdescription"
                 />
-                <meta property="og:image" content="/og.png" />
+                <meta property="og:image" content="/assets/og.png" />
+
+                {/* Twitter */}
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:title" content={headline} />
+                <meta name="twitter:creator" content="@Zach_Sents" />
+                <meta name="twitter:image" content="/assets/og.png" />
+                <meta name="twitter:description" content={description} />
 
                 {/* Assets */}
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
